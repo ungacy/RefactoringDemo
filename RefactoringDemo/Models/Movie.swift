c6b4fd70868d4ea45588eeb2846096c1f68aabe7
ungacy <ungacy@126.com>
2017-03-08 16:12:36 +0800

    Move Method
    move getCharge to Price

--- RefactoringDemo/Models/Movie.swift
+++ RefactoringDemo/Models/Movie.swift
@@ -45,23 +45,7 @@ class Movie {
     }
     
     func getCharge(daysRented: Int) -> Double {
-        var result: Double = 0.0
-        switch priceCode {
-        case .REGULAR:
-            result += 2
-            if daysRented > 2 {
-                result += Double((daysRented - 2)) * 1.5
-            }
-        case .NEW_RELEASE:
-            result += Double(daysRented) * 3
-        case .CHILDREN:
-            result += 1.5
-            if daysRented > 3 {
-                result += Double((daysRented - 3)) * 1.5
-            }
-        }
-        
-        return result
+        return price!.getCharge(daysRented: daysRented)
     }
     
     func getFrequentRenterPoints(daysRented: Int) -> Int {
--- RefactoringDemo/Models/Price.swift
+++ RefactoringDemo/Models/Price.swift
@@ -10,6 +10,29 @@ import Foundation
 
 public protocol Price {
     func priceCode() -> PriceCodeType
+    func getCharge(daysRented: Int) -> Double
+}
+
+public extension Price {
+    public func getCharge(daysRented: Int) -> Double {
+        var result: Double = 0.0
+        switch priceCode() {
+        case .REGULAR:
+            result += 2
+            if daysRented > 2 {
+                result += Double((daysRented - 2)) * 1.5
+            }
+        case .NEW_RELEASE:
+            result += Double(daysRented) * 3
+        case .CHILDREN:
+            result += 1.5
+            if daysRented > 3 {
+                result += Double((daysRented - 3)) * 1.5
+            }
+        }
+        
+        return result
+    }
 }
 
 public class ChildrenPrice : Price {
